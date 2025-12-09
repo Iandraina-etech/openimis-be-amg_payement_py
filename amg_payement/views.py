bcdbb6dffcdced40abfd11eda8d97779d8423f36
Iandraina <217048206+Iandraina-etech@users.noreply.github.com>
2025-12-09 11:33:21 +0300

    Update views.py

--- amg_payement/views.py
+++ amg_payement/views.py
@@ -177,10 +177,10 @@ def redirect_cancel(request):
 def api_notify(request):
 
     # IP whitelist check
-    # remote_addr = request.META.get('REMOTE_ADDR') or request.META.get('HTTP_X_FORWARDED_FOR', '').split(',')[0].strip()
-    # if remote_addr and not is_ip_whitelisted(remote_addr):
-    #     logger.warning(f"Notify IP non autorisée: {remote_addr}")
-    #     return HttpResponseForbidden("IP non autorisée")
+    remote_addr = request.META.get('REMOTE_ADDR') or request.META.get('HTTP_X_FORWARDED_FOR', '').split(',')[0].strip()
+    if remote_addr and not is_ip_whitelisted(remote_addr):
+        logger.warning(f"Notify IP non autorisée: {remote_addr}")
+        return HttpResponseForbidden("IP non autorisée")
 
     payload = request.GET
 
